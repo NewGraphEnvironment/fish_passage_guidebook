21ccc74e06b92e6c1cd2707d99957bea1f356df6
almac2022 <al@newgraphenvironment.com>
2024-08-23 13:47:51 -0700

    provide comments to clarify correctly

--- scripts/photo_convert.R
+++ scripts/photo_convert.R
@@ -1,5 +1,6 @@
-# resize a portrait image to display nicely in report
-photos <- c("timestamp_settings.jpeg",
+# resize images to save space. Portrait need the out.width set to out.width='50%' in the chunk though
+photos <- c("timestamp_settings_access.jpg",
+            "timestamp_settings.jpeg",
             "methods_mobile.jpg",
             "more_sections.jpg",
             "photos_mobile.jpg")
@@ -8,10 +9,13 @@ paths <- paste0(
   fs::path(here::here(),  "fig", "fieldwork", photos)
 )
 
+size = "777.6x1036.8!"
+
+
 paths |>
   purrr::map(
     fpr::fpr_photo_resize_convert,
-                              size = "777.6x1036.8!",
-                              path = fs::path("fig", "fieldwork")
+                              size = size,
+                              path = fs::path("fig", "fieldwork", "resized")
     )
 
